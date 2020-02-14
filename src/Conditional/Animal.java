9f3c16451e0a912103ce9a8576638d5841fd4e72
nawaraj thapa <nawarajthapa07@gmail.com>
2020-02-13 21:37:26 -0700

    feb 14

--- src/Conditional/Animal.java
+++ src/Conditional/Animal.java
@@ -1,10 +1,23 @@
 package Conditional;
 
 public class Animal {
+	String name;
+	int age;
+	String color;
+	
+	Animal(String n,int a,String c) {
+		name=n;
+		age=a;
+		color=c;
+		
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	void displayanimal(a) {
+		System.out.println("The name is "+a.name+" age is "+a.age+" and the color is "+a.color);
+		
+		
+	}
+		
 	}
 
 }
